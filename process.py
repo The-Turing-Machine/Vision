9de897cd8d5c1a4a7b920c795cfae3ab2d97aa55
ayush <ayushkumarsingh97@gmail.com>
2016-08-20 12:45:07 +0530

    process added

--- process.py
+++ process.py
@@ -17,34 +17,32 @@ def noun(answer):
         text_to_speech.get_speech("Are you looking for"+ words[0])
         answer = speech_to_text.stt()
         print answer
-        if "yes" in answer.lower() or "ya" in answer.lower() or "yup" in answer.lower():
+        if "yes" in answer.lower() or "ye" in answer.lower():
             pass
     elif(len(words)>1):
+        a = ""
+        a = " or ".join(words)
+        print a
 
-        for i in range(len(words)):
-            a = words[i]+" or"
-        print a
-        a = a[:-2]
-        print a
         text_to_speech.get_speech("What are you looking for"+ a)
         answer = speech_to_text.stt()
-        for i in len(words):
-            if words[i] in answer.lower():
-                pass
+        if answer in words:
+            pass
 
 
 
 
 #vision
 
-
-while(True):
-    text_to_speech.get_speech("command")
-    answer = speech_to_text.stt()
-    print answer
-    if "vision" in answer.lower():
-        text_to_speech.get_speech("How may i help you ?")
+def main():
+    while(True):
+        text_to_speech.get_speech("command !")
         answer = speech_to_text.stt()
-        noun(answer)
-    else:
-        text_to_speech.get_speech("Oops! Didn't catch that,pardon!")
+        print answer
+        if "vision" in answer.lower():
+            text_to_speech.get_speech("How may i help you ?")
+            answer = speech_to_text.stt()
+            noun(answer)
+        else:
+            text_to_speech.get_speech("Oops! Didn't catch that,pardon!")
+main()
